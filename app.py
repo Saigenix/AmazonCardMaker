c4803947dcac19c262a606561fc1c77f450e79c3
saigenix <sainathmahindrakar@gmail.com>
2022-02-09 23:08:55 +0530

    update2

--- app.py
+++ app.py
@@ -15,10 +15,15 @@ def home():
 def result():
     output = request.form.to_dict()
     print(output)
-    name = output["name"]
+    modelFea = output["modelFea"]
+    modelImg = output["modelImg"]
+    modelName = output["modelName"]
+    modelPrice = output["modelPrice"]
+    modelDes = output["modelDes"]
+    modelLink = output["modelLink"]
 
 
-    return render_template('index.html', name = name)
+    return render_template('index.html', modelFea = modelFea, modelImg = modelImg, modelName = modelName, modelPrice = modelPrice, modelDes = modelDes, modelLink = modelLink)
     
 
 
